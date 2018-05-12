4b7f46fb0e8e0a1488cc0673e96ecbfe95777e25
Andrew Milne <andymilne@tonalcentre.org>
2018-05-12 16:05:07 +1000

    Initial commit

---
+++ fSetRoughness.m
@@ -0,0 +1,85 @@
+function r = fSetRoughness(x_f,x_w,pNorm,isAve)
+%FSETROUGHNESS The roughness of partials with frequencies 'x_f' and weights 'x_w'.
+%
+%   r = fSetRoughness(x_f, x_w, pNorm, isAve): The total roughness of the set
+%   of partials with frequencies 'x_f' and weights 'x_w'.
+%
+%   pNorm sets the norm by which the total roughness is calculated from the
+%   "subroughnesses" of each partial pair. It defaults to 1, which is simple
+%   summation.
+%   
+%   isAve = 0 or 1 - when 1, the total roughness is divided by the total number
+%   of pairs of partials. When pNorm is 1, this gives the expected roughness of
+%   a partial pair. The default is 0.
+%
+%   This function uses Bill Sethares' (1993) parameterization of Plomp-Levelt's
+%   (1965) empirical dissonance curve.
+%
+%   Plomp, R. and Levelt, W. J. M. (1965). Tonal consonance and critical
+%   bandwidth. The Journal of the Acoustical Society of America, 38(4):548-560.
+%
+%   Sethares, W. A. (1993). Local consonance and the relationship between
+%   timbre and scale. The Journal of the Acoustical Society of America,
+%   94(3):1218-1228.
+%
+%   By Andrew J. Milne, The MARCS Institute, Western Sydney University
+%
+%   See also: pSetSpectralEntropy
+
+if nargin < 4
+    isAve = 0;
+    pNorm = 1;
+end
+if nargin < 3
+    pNorm = 1;
+end
+
+if pNorm <=0
+    error('pNorm must be a positive number')
+end
+if ~isequal(isAve,0) && ~isequal(isAve,1) 
+    error('isAve must be 0 or 1')
+end
+
+%% Fixed parameters
+Dstar = 0.24; 
+S1 = 0.0207; 
+S2 = 18.96; 
+C1 = 5; 
+C2 = -5;
+A1 = -3.51; 
+A2 = -5.75; 
+
+%% Variables
+x_f = x_f(:);
+x_w = x_w(:);
+
+fDiff = x_f - x_f';
+fDiff = fDiff(:);
+
+fMin = min(x_f,x_f');
+fMin = fMin(:);
+
+aMin = min(x_w,x_w');
+aMin = aMin(:);
+
+validInd = fDiff>0;
+fMin = fMin(validInd);
+aMin = aMin(validInd);
+fDiff = fDiff(validInd);
+
+fMid = fMin + fDiff/2; % This may be useful when attempting to take into 
+% account modifications suggested in Parncutt (2006) and Vos (2006)
+% commentaries on Mashinter (2006) regarding summations within and between CBs.
+
+%% Calculate roughness
+allS = Dstar./(S1*fMin + S2);
+allRough = aMin.*(C1*exp(A1*allS.*fDiff) + C2*exp(A2*allS.*fDiff));
+
+if isAve == 1
+    r = (1/numel(fDiff))*sum(allRough.^pNorm)^(1/pNorm);
+else
+    r = sum(allRough.^pNorm)^(1/pNorm);
+end
+
+end
---
+++ pSetSpectralEntropy.m
@@ -0,0 +1,56 @@
+function h = pSetSpectralEntropy(set_p,set_w,spec_p,spec_w,...
+                                 sigma,kerLen,isPer,period)
+
+%PSETSPECTRALENTROPY Spectral entropy of a pitch (class) multi set given a
+%   spectrum.
+%
+%   h = pSetSpectralEntropy(scalePc, scaleWt, spectrumPc, spectrumWt): The
+%   spectral entropy of the gaussian smoothed spectra produced by a pitch
+%   (class) multiset (e.g., a scale or chord) where each pitch stands for a
+%   spectrum of pitches given by 'spectrum'. Pitch sets with higher entropy
+%   have fewer overlapping partials, hence this serves as a model of the
+%   multiset's overall spectral complexity, possibly dissonance.
+%
+%   scale_p are the pitches of the multiset in cents (pitch classes when
+%   isPer==1).
+%
+%   scale_w are the associated pitch weights: all ones, if there is only one
+%   zero entered or empty.
+%
+%   spec_p are the spectral pitches in cents: harmonics, if less than three
+%   variables are entered.
+%
+%   spec_w are the associated spectral weights: all ones, if less than four
+%   variables are entered.
+%
+%   For explanations of the remaining arguments, see expectationTensor.
+%
+%   By Andrew J. Milne, The MARCS Institute, Western Sydney University
+%
+%   See also: fSetRoughness
+
+%% Fixed parameters (not intended to be optimized to the data)
+if nargin < 7
+    period = 1200; % size of period in pitch units
+end
+if nargin < 6
+    kerLen = 6; % width in standard deviations of smoothing kernel
+end
+if nargin < 5
+    sigma = 6;
+end
+
+r = 1; % r-ads considered
+isRel = 0; % transpositional invariance (1) or not (0)
+
+%% Spectralize the scale pitches
+[~,set_p,set_w] = spectralize(set_p,set_w,spec_p,spec_w);
+set_p = set_p(:);
+set_w = set_w(:);
+
+%% Absolute monad vector and its entropy
+setExpVec = expectationTensor(set_p, set_w, sigma, kerLen, ...
+                              r, isRel, isPer, period);
+h = histEntropy(setExpVec(:));
+
+end

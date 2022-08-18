e3e57969de03102efa29dd4e0787945c4e4d8d2f
Andrew Milne <andymilne@tonalcentre.org>
2022-08-18 10:15:30 +1000

    add thresh argument

--- peakPicker.m
+++ peakPicker.m
@@ -1,5 +1,5 @@
 function [pks_p,pks_w,absDft_f,smoothX_p,pVals] ...
-        = peakPicker(x_t, Fs, sigma, fRef, doPlot)
+        = peakPicker(x_t, Fs, sigma, thresh, fRef, doPlot)
 
 %PEAKPICKER Convert time-domain signal to log-f spectrum, smooth, find peaks.
 %
@@ -37,14 +37,17 @@ x_f = fft(x_t)/nSamples;
 X = dft2sss(x_f,Fs,'MP','FRef',fRef);
 
 %% Convert to absolute pitch expectation vector
-x_p = X.MP;
-x_w = abs(X.Phasors);
+x_p = X.MP; % the pitches (cents relative to fRef) of the entries in x_w
+x_w = abs(X.Phasors); % the magnitudes of the pitches in x_p
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 absDft_f = x_w;
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
-x_p(x_w < 0.0001) = []; % remove almost zero values
-x_w(x_w < 0.0001) = []; % remove almost zero values
+% Remove almost-zero values below thresh. A sensible thresh value is about
+% 60dB below the signal's max because these are likely to be perceptually
+% masked (inaudible) and are possible sectral leakage artefacts
+x_p(x_w < thresh) = []; 
+x_w(x_w < thresh) = [];
 
 r = 1;
 isRel = 0;
